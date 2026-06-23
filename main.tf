670e7ebebc59aa8c30dd8d056a893c158bb22a14
Nikunj Patel <70764321+nikunjpatel19@users.noreply.github.com>
2026-06-23 17:19:57 -0400

    Add AWS provider and resources for S3 and EC2

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+region = "ca-central-1"
+}
+
+resource "aws_s3_bucket" "bucket01" {
+bucket = "githubactions-bucket"
+}
+
+resource "aws_instance" "my_server" {
+  ami           = "ami-0d19d7a9bc91b3550" 
+  instance_type = "t3.micro"             
+
+  tags = {
+    Name = "Github Actions EC2"
+  }
+}
